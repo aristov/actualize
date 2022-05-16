44504734cad5bbd15cb7a1e033b3d24ebbebc8e8
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-16 09:22:30 +0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const { NODE_ENV } = process.env
+const JS_EXT = NODE_ENV === 'production' ? '.min.js' : '.js'
+
+module.exports = {
+  mode : NODE_ENV || 'none',
+  entry : './lib/index.js',
+  output : {
+    filename : 'actualize' + JS_EXT,
+    library : {
+      name : 'actualize',
+      type : 'umd',
+    },
+    globalObject : 'this',
+  },
+}
